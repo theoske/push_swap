dbbf9f3f90a5dce924352c2637dea4ec8a1ec4eb
Theo Kempf elbaz <tkempf-e@k0r4p1.42mulhouse.fr>
2022-05-11 17:35:30 +0200

    maj

--- push_swap.c
+++ push_swap.c
@@ -6,13 +6,26 @@
 /*   By: tkempf-e <tkempf-e@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 14:34:37 by tkempf-e          #+#    #+#             */
-/*   Updated: 2022/05/11 16:06:04 by tkempf-e         ###   ########.fr       */
+/*   Updated: 2022/05/11 17:35:19 by tkempf-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>//
+
+
+unsigned int	ft_strlen(const char *s)
+{
+	unsigned int	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i])
+		i++;
+	return (i);
+}
 
 char	*swap(char *tab) // takes stacks a or b
 {
@@ -26,32 +39,32 @@ char	*swap(char *tab) // takes stacks a or b
 	return (tab);
 }
 
-char	**push(char *gifter, char *receiver) //push number 
-{
-	char	*tab[2];
-	int		i;
+// char	**push(char *gifter, char *receiver) //push number 
+// {
+// 	char	*tab[2];
+// 	int		i;
 
-	if (!gifter)
-		return (tab);
-	tab[2] = 0;
-	tab[0] = malloc(ft_strlen(gifter) - 1);
-	tab[1] = malloc(ft_strlen(receiver) + 1);
-	tab[1][0] = gifter[0];
-	while (receiver && receiver[i])
-	{
-		tab[1][i + 1] = receiver[i];
-		i++;
-	}
-	tab[1][i + 1] = 0;
-	i = 0;
-	while (gifter[i + 1])
-	{
-		tab[0][i] = gifter[i + 1];
-		i++;
-	}
-	tab[0][i] = 0;
-	return (tab);
-}
+// 	if (!gifter)
+// 		return (tab);
+// 	tab[2] = 0;
+// 	tab[0] = malloc(ft_strlen(gifter) - 1);
+// 	tab[1] = malloc(ft_strlen(receiver) + 1);
+// 	tab[1][0] = gifter[0];
+// 	while (receiver && receiver[i])
+// 	{
+// 		tab[1][i + 1] = receiver[i];
+// 		i++;
+// 	}
+// 	tab[1][i + 1] = 0;
+// 	i = 0;
+// 	while (gifter[i + 1])
+// 	{
+// 		tab[0][i] = gifter[i + 1];
+// 		i++;
+// 	}
+// 	tab[0][i] = 0;
+// 	return (tab);
+// }
 
 char	*rotate(char *str)
 {
@@ -64,14 +77,48 @@ char	*rotate(char *str)
 		return (NULL);
 	endchar = str[0];
 	i = ft_strlen(str) - 1;
-	swapper = str[i - 1];
-	str[i - 1] = str[i];
-	i -= 2;
-	swapper2 = str[i];
-	str[i] = swapper;
+	swapper = str[i];
 	while (i > 0)
 	{
-		swapper = str[i - 1];
-		str[i - 1] = str[i];
+		swapper2 = str[i];
+		str[i] = swapper;
+		i--;
+		swapper = str[i];
+		str[i] = swapper2;
+		i--;
 	}
+	str[ft_strlen(str) - 1] = endchar;
+	return (str);
+}
+
+char	*rev_rotate(char *str)
+{
+	int		i;
+	char	swapper;
+	char	swapper2;
+	char	startchar;
+
+	if (!str)
+		return (NULL);
+	startchar = str[ft_strlen(str) - 1];
+	i = 0;
+	swapper = str[i];
+	while (i > 0)
+	{
+		swapper2 = str[i];
+		str[i] = swapper;
+		i--;
+		swapper = str[i];
+		str[i] = swapper2;
+		i--;
+	}
+	str[ft_strlen(str) - 1] = endchar;
+	return (str);
+}
+
+int main()
+{
+	char str[] = "123456";
+	printf(" %s ", rotate(str));
+	return 0;
 }
